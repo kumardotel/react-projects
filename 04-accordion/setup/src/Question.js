bf97eb7b76c346c0877eb30e15527dff561ef5de
kumardotel <kumardotel18@gmail.com>
2021-06-30 14:11:25 +0545

    intial react projects

--- 04-accordion/setup/src/Question.js
+++ 04-accordion/setup/src/Question.js
@@ -1,7 +1,32 @@
-import React, { useState } from 'react';
-import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
-const Question = () => {
-  return <h2>question component</h2>;
+import React, { useState } from "react";
+import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
+const Question = ({ id, title, info }) => {
+  const [plus, setPlus] = useState(true);
+
+  const changeIconPlus = () => {
+    setPlus(!plus);
+  };
+  const changeIconMinus = () => {
+    setPlus(!plus);
+  };
+  return (
+    <article className="question">
+      <header>
+        <h4>{title}</h4>
+        {plus ? (
+          <button className="btn" onClick={changeIconPlus}>
+            <AiOutlinePlus />
+          </button>
+        ) : (
+          <button className="btn" onClick={changeIconMinus}>
+            <AiOutlineMinus />
+          </button>
+        )}
+      </header>
+
+      <p>{plus ? null : info}</p>
+    </article>
+  );
 };
 
 export default Question;
